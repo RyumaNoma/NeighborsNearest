08a23547d3e23446aa1815782d40adb491b40bc4
RyumaNoma <40262464+RyumaNoma@users.noreply.github.com>
2019-08-22 15:03:47 +0900

    Update NeighborsNearest.py

--- NeighborsNearest.py
+++ NeighborsNearest.py
@@ -1,7 +1,3 @@
-from sklearn.datasets import load_iris
-from sklearn.model_selection import train_test_split
-from sklearn.neighbors import KNeighborsClassifier
-
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -45,24 +41,3 @@ class NeighborsNearest:
         count += 1
         
     return count / len(y_test)
-
-# アヤメ分類問題を用いて正しく動作しているか確認する
-# アヤメデータセットの準備
-iris = load_iris()
-X_train, X_test, y_train, y_test = train_test_split(iris.data, iris.target, random_state=0)
-
-# テストデータ[5.8, 2.8, 5.1, 2.4]を予想してみる（答えは[2]）
-
-# scikit-learnに実装されているK近傍法を利用する
-knn = KNeighborsClassifier(n_neighbors=1)
-knn.fit(X_train, y_train)
-print(knn.predict([[5.8, 2.8, 5.1, 2.4]])) #=> [2]
-
-# X_train, y_trainを用いて学習する
-k = NeighborsNearest()
-k.fit(X_train, y_train)
-print(k.predict([5.8, 2.8, 5.1, 2.4])) #=>　2
-
-# X_test, y_testを予想する
-print(knn.score(X_test, y_test)) #=> 0.9736842105263158
-print(k.score(X_test, y_test)) #=> 0.9736842105263158
